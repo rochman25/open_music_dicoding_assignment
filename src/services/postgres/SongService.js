475be471ea37704569f0249fb39bddf70c0ba418
rochman25 <zaenur.rochman98@gmail.com>
2021-07-02 13:15:02 +0700

    update song service

---
+++ src/services/postgres/SongService.js
@@ -0,0 +1,27 @@
+const { nanoid } = require('nanoid');
+const { Pool } = require('pg');
+class SongService {
+ constructor() {
+    this._pool = new Pool();
+ }
+ 
+ async addSong({title, year, performer, genre, duration}) {
+   const id = nanoid(16);
+   const insertedAt = new Date().toISOString();
+   const updatedAt = insertedAt;
+
+   const query = {
+    text: 'INSERT INTO songs VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id',
+    values: [id, title, year, performer, genre, duration, insertedAt, updatedAt],
+   };
+
+   const result = await this._pool.query(query);
+
+   if (!result.rows[0].id) {
+    throw new InvariantError('Lagu gagal ditambahkan');
+   }
+
+   return result.rows[0].id;
+ }
+
+}
---
+++ src/utils/index.js
@@ -0,0 +1,21 @@
+const mapDBToModel = ({
+    id,
+    title,
+    year,
+    performer,
+    genre,
+    duration,
+    insertedAt,
+    updatedAt
+}) => ({
+    id,
+    title,
+    year,
+    performer,
+    genre,
+    duration,
+    insertedAt,
+    updatedAt,
+});
+
+module.exports = { mapDBToModel };
