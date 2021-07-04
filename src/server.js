639ac28c675b98def3b9d40793b3eb93bfb1ae5a
Zaenurrochman <zaenur.rochman98@gmail.com>
2021-07-04 10:57:52 +0700

    add exceptions
    - add client error exception
    - add invariant error exception
    - add not found error exception

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 const Hapi = require('@hapi/hapi');
 // const routes = require('./routes');
- 
- 
+
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
@@ -12,11 +11,11 @@ const init = async () => {
       },
     },
   });
- 
-//   server.route(routes);
- 
+
+  //   server.route(routes);
+
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
- 
+
 init();
--- src/services/postgres/SongService.js
+++ src/services/postgres/SongService.js
@@ -1,27 +1,33 @@
+/* eslint-disable no-underscore-dangle */
 const { nanoid } = require('nanoid');
 const { Pool } = require('pg');
+const InvariantError = require('../../exceptions/InvariantError');
+
 class SongService {
- constructor() {
+  constructor() {
     this._pool = new Pool();
- }
- 
- async addSong({title, year, performer, genre, duration}) {
-   const id = nanoid(16);
-   const insertedAt = new Date().toISOString();
-   const updatedAt = insertedAt;
+  }
 
-   const query = {
-    text: 'INSERT INTO songs VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id',
-    values: [id, title, year, performer, genre, duration, insertedAt, updatedAt],
-   };
+  async addSong({
+    title, year, performer, genre, duration,
+  }) {
+    const id = nanoid(16);
+    const insertedAt = new Date().toISOString();
+    const updatedAt = insertedAt;
 
-   const result = await this._pool.query(query);
+    const query = {
+      text: 'INSERT INTO songs VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id',
+      values: [id, title, year, performer, genre, duration, insertedAt, updatedAt],
+    };
 
-   if (!result.rows[0].id) {
-    throw new InvariantError('Lagu gagal ditambahkan');
-   }
+    const result = await this._pool.query(query);
 
-   return result.rows[0].id;
- }
+    if (!result.rows[0].id) {
+      throw new InvariantError('Lagu gagal ditambahkan');
+    }
 
+    return result.rows[0].id;
+  }
 }
+
+module.exports = SongService;
