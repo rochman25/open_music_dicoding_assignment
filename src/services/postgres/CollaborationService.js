1014b518cc59fffb91c617dcdce5628cb4a7def8
Zaenurrochman <zaenur.rochman98@gmail.com>
2021-08-15 11:46:41 +0700

    add delete collboration
    - add delete collboration

--- src/services/postgres/CollaborationService.js
+++ src/services/postgres/CollaborationService.js
@@ -23,6 +23,32 @@ class CollaborationService {
     }
     return result.rows[0].id;
   }
+
+  async deleteCollaboration(playlistId, userId) {
+    const query = {
+      text: 'DELETE FROM collaborations WHERE playlist_id = $1 AND user_id = $2 RETURNING id',
+      values: [playlistId, userId],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new InvariantError('Kolaborasi gagal dihapus');
+    }
+  }
+
+  async verifyCollaborator(playlistId, userId) {
+    const query = {
+      text: 'SELECT * FROM collaborations WHERE playlist_id = $1 AND user_id = $2',
+      values: [playlistId, userId],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new InvariantError('Kolaborasi gagal diverifikasi');
+    }
+  }
 }
 
 module.exports = CollaborationService;
